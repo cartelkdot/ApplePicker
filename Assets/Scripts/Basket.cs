401e3f4f766279158c3303af5a43124eb80e3ac8
cartelkdot <47113944+cartelkdot@users.noreply.github.com>
2019-09-12 09:23:31 -0400

    added basket and apple picker.
    See title.

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //The Camera's position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePod3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x positionn of this Basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePod3D.x;
+        this.transform.position = pos;
+
+
+    }
+}
